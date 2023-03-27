c9882c835a18939afee5c8c6606f777ed26a0ce3
João Gabriel Vieira <joaogvieira21@gmail.com>
2023-03-27 19:16:14 -0300

    Criado o endpoint, handler e model de User

--- server/index.js
+++ server/index.js
@@ -3,16 +3,29 @@ const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const PORT = process.env.PORT;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const express = require('express')
 const app = express()
+const userRoutes = require('./routes/User_r')
+
+//Congif JSON response
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Rota usuários
+app.use(userRoutes)
 
 
+mongoose
+    .connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.or07rye.mongodb.net/?retryWrites=true&w=majority`)
+    .then((data) => {
+        app.listen(PORT, () => {
+            console.log("Server is running on PORT " + PORT )
+        })
+    }).catch((err) => {
+        console.log(err)
+    })
 
 
-app.use('/', (req, res) => {
-    res.send("Primeira pagina!")
-})
-
-app.listen(PORT, () => {
-    console.log("Server is running on PORT " + PORT )
-})
